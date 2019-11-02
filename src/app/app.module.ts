783309b3d7fcd80b2699493ab1aa88f60542d918
Jolene Le <renreine4x1@gmail.com>
2019-11-02 17:06:39 -0400

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,18 +6,41 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+=======
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { MessagesComponent }    from './messages/messages.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+>>>>>>> Stashed changes
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent
   ],
+<<<<<<< Updated upstream
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+=======
+  bootstrap: [ AppComponent ]
+>>>>>>> Stashed changes
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,20 +15,34 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
+<<<<<<< Updated upstream
 
   heroes = HEROES;
   selectedHero: Hero;
 
   constructor() { }
+=======
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+>>>>>>> Stashed changes
   ngOnInit() {
 
   }
 
+<<<<<<< Updated upstream
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
 
+=======
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+>>>>>>> Stashed changes
 }
 
 
